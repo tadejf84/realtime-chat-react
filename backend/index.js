94f8275f2dabe71f2e4565312eb27fa6119e9a73
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-03-27 18:09:44 +0100

    user joins room functionality on backend

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 const router = require('./router');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 // Define PORT
 const PORT = process.env.PORT || 5000;
 
+// Start express app
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -13,8 +15,18 @@ const io = socketio(server);
 io.on('connection', (socket) => {
     console.log('We have a new connection');
 
-    socket.on('join', ({ name, room }) => {
-        console.log(name, room);
+    socket.on('join', ({ name, room }, callback) => {
+
+        // Add user to users arr
+        const { error, user } = addUser({ id: socket.id, name, room });
+
+        // If any error, return
+        if(error) return callback(error);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the ${user.room}` })
+
+        // User joins room
+        socket.join(user.room);
     });
 
     socket.on('disconnect', () => console.log('user has left!'));
